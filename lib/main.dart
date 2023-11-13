4da47ce64617403ad58f1e9ee596000494e7f593
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-11-13 13:13:38 +0530

    Changed the layout of the app

--- lib/main.dart
+++ lib/main.dart
@@ -146,34 +146,40 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Tracking Live Location of driver",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Column(children: [
-                  Text(
-                    "Longtide: $lon",
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  Icon(
+                    Icons.location_pin,
+                    size: 60,
+                    color: Colors.blue.shade400,
                   ),
-                  Text(
-                    "Latitude: $lat",
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  const SizedBox(height: 20),
                   Text(
                     "Current Address: $address",
                     style: const TextStyle(
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
                     ),
+                    textAlign: TextAlign.center,
                   ),
                 ]),
               ],
